3bf06c93cf05cd39ee42b1076c4d9c95e3746c98
chris_mint <chris.achinga@student.moringaschool.com>
2022-10-04 02:20:15 +0300

    completed

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,15 +1,19 @@
-import React from "react";
+import React from 'react'
 
 function ProjectItem({ name, about, technologies }) {
+
   return (
-    <div className="project-item">
+    <div className='project-item'>
       <h3>{name}</h3>
       <p>{about}</p>
-      <div className="technologies">
+      <div className='technologies'>
         {/* render a <span> for each technology in the technologies array */}
+        {technologies.map((tech, index) => {
+          return <span key={index}>{tech}</span>
+        })}
       </div>
     </div>
-  );
+  )
 }
 
-export default ProjectItem;
+export default ProjectItem
